54d7dd286f317608a8d6f4fa47fa41649aa8b250
pjw <pjw>
2022-05-31 10:17:37 +0900

    2022-05-31  1.소스정리

--- src/main/java/com/pjw/pjwEgove/cmm/dao/CmmDao.java
+++
@@ -1,5 +0,0 @@
-package com.pjw.pjwEgove.cmm.dao;
-
-public class CmmDao {
-
-}
---
+++ src/main/java/com/pjw/pjwEgove/cmm/mapper/CmmMapper.java
@@ -0,0 +1,8 @@
+package com.pjw.pjwEgove.cmm.mapper;
+
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
+
+@Mapper
+public class CmmMapper {
+
+}
